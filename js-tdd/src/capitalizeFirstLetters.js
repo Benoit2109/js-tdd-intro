14c614fa672f5a51c6952858e6afa2383a8d7491
Benoit LE BOURDONNEC <benoitlebourdonnec@MacBook-Pro-2.local>
2021-01-30 10:27:10 +0100

    second quest

--- js-tdd/src/capitalizeFirstLetters.js
+++ js-tdd/src/capitalizeFirstLetters.js
@@ -1,4 +1,3 @@
-const assert = require("assert");
 
 const capitalizeFirstLetters = (input) =>
   input.length > 0
@@ -7,12 +6,6 @@ const capitalizeFirstLetters = (input) =>
         .map((e) => e[0].toUpperCase() + e.slice(1))
         .join(" ")
     : "";
-    
-assert.strictEqual(typeof capitalizeFirstLetters, "function");
-assert.strictEqual(capitalizeFirstLetters("javaScript"), "JavaScript");
-assert.strictEqual(capitalizeFirstLetters("j"), "J");
-assert.strictEqual(
-  capitalizeFirstLetters("javaScript est un langage de programation"),
-  "JavaScript Est Un Langage De Programation"
-);
-assert.strictEqual(capitalizeFirstLetters(""), "");
+
+    module.exports = capitalizeFirstLetters;
+
--- js-tdd/src/test.js
+++ js-tdd/src/test.js
@@ -1,11 +1,23 @@
 const assert = require("assert");
-const { capitalizeFirstLetters } = require("./capitalizeFirstLetters") ;
+const capitalizeFirstLetters = require("./capitalizeFirstLetters");
 
-assert.strictEqual(typeof capitalizeFirstLetters, "function");
-assert.strictEqual(capitalizeFirstLetters("javaScript"), "JavaScript");
-assert.strictEqual(capitalizeFirstLetters("j"), "J");
-assert.strictEqual(
-  capitalizeFirstLetters("javaScript est un langage de programation"),
-  "JavaScript Est Un Langage De Programation"
-);
-assert.strictEqual(capitalizeFirstLetters(""), "");
+describe("capitalizeFirstLetters", () => {
+  it("is a function", () => {
+    assert.strictEqual(typeof capitalizeFirstLetters, "function");
+  });
+  it("transform javaScript correctly", () => {
+    assert.strictEqual(capitalizeFirstLetters("javaScript"), "JavaScript");
+  });
+  it("works for a 1-character string", () => {
+    assert.strictEqual(capitalizeFirstLetters("j"), "J");
+  });
+  it("works for a N-word string", () => {
+    assert.strictEqual(
+      capitalizeFirstLetters("javaScript est un langage de programation"),
+      "JavaScript Est Un Langage De Programation"
+    );
+  });
+  it("works for an empty string", () => {
+    assert.strictEqual(capitalizeFirstLetters(""), "");
+  });
+});
